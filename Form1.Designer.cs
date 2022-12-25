3ab5227d0af6a2da7ef05b30a59fa102c48350a9
ramylawandyfouad <68560512+ramylawandyfouad@users.noreply.github.com>
2022-12-25 10:43:44 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pnlTop = new System.Windows.Forms.Panel();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.tltMain = new System.Windows.Forms.ToolTip(this.components);
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblBirthday = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.dtpBirthday = new System.Windows.Forms.DateTimePicker();
+            this.lblToday = new System.Windows.Forms.Label();
+            this.lblTodayResult = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.pnlTop.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlTop
+            // 
+            this.pnlTop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(26)))));
+            this.pnlTop.Controls.Add(this.btnExit);
+            this.pnlTop.Controls.Add(this.lblHeader);
+            this.pnlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTop.Location = new System.Drawing.Point(0, 0);
+            this.pnlTop.Name = "pnlTop";
+            this.pnlTop.Size = new System.Drawing.Size(582, 39);
+            this.pnlTop.TabIndex = 0;
+            this.pnlTop.DoubleClick += new System.EventHandler(this.pnlTop_DoubleClick);
+            this.pnlTop.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlTop_MouseDown);
+            this.pnlTop.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnlTop_MouseMove);
+            this.pnlTop.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pnlTop_MouseUp);
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lblHeader.Font = new System.Drawing.Font("Andalus", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeader.ForeColor = System.Drawing.Color.White;
+            this.lblHeader.Location = new System.Drawing.Point(0, 0);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(169, 36);
+            this.lblHeader.TabIndex = 0;
+            this.lblHeader.Text = "Age Calculator";
+            this.lblHeader.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tltMain
+            // 
+            this.tltMain.AutomaticDelay = 200;
+            this.tltMain.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Andalus", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.White;
+            this.lblName.Location = new System.Drawing.Point(120, 103);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(66, 31);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Name";
+            this.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblBirthday
+            // 
+            this.lblBirthday.AutoSize = true;
+            this.lblBirthday.Font = new System.Drawing.Font("Andalus", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBirthday.ForeColor = System.Drawing.Color.White;
+            this.lblBirthday.Location = new System.Drawing.Point(119, 150);
+            this.lblBirthday.Name = "lblBirthday";
+            this.lblBirthday.Size = new System.Drawing.Size(91, 31);
+            this.lblBirthday.TabIndex = 3;
+            this.lblBirthday.Text = "Birthday";
+            this.lblBirthday.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(259, 110);
+            this.txtName.MaxLength = 30;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(274, 22);
+            this.txtName.TabIndex = 4;
+            this.tltMain.SetToolTip(this.txtName, "Enter Your Name");
+            // 
+            // dtpBirthday
+            // 
+            this.dtpBirthday.Location = new System.Drawing.Point(259, 159);
+            this.dtpBirthday.Name = "dtpBirthday";
+            this.dtpBirthday.Size = new System.Drawing.Size(274, 22);
+            this.dtpBirthday.TabIndex = 5;
+            this.dtpBirthday.Value = new System.DateTime(2022, 1, 1, 9, 52, 0, 0);
+            this.dtpBirthday.ValueChanged += new System.EventHandler(this.dtpBirthday_ValueChanged);
+            // 
+            // lblToday
+            // 
+            this.lblToday.AutoSize = true;
+            this.lblToday.Font = new System.Drawing.Font("Andalus", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblToday.ForeColor = System.Drawing.Color.White;
+            this.lblToday.Location = new System.Drawing.Point(119, 61);
+            this.lblToday.Name = "lblToday";
+            this.lblToday.Size = new System.Drawing.Size(67, 31);
+            this.lblToday.TabIndex = 6;
+            this.lblToday.Text = "Today";
+            this.lblToday.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTodayResult
+            // 
+            this.lblTodayResult.AutoSize = true;
+            this.lblTodayResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTodayResult.ForeColor = System.Drawing.Color.White;
+            this.lblTodayResult.Location = new System.Drawing.Point(254, 63);
+            this.lblTodayResult.Name = "lblTodayResult";
+            this.lblTodayResult.Size = new System.Drawing.Size(20, 29);
+            this.lblTodayResult.TabIndex = 7;
+            this.lblTodayResult.Text = ".";
+            this.lblTodayResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.Color.Black;
+            this.btnCalculate.ForeColor = System.Drawing.Color.White;
+            this.btnCalculate.Location = new System.Drawing.Point(36, 229);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(118, 45);
+            this.btnCalculate.TabIndex = 8;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Andalus", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.Color.White;
+            this.lblResult.Location = new System.Drawing.Point(195, 213);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(24, 27);
+            this.lblResult.TabIndex = 9;
+            this.lblResult.Text = "...";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.Images;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 56);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(107, 125);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnExit.Image = global::WindowsFormsApp1.Properties.Resources.Exit;
+            this.btnExit.Location = new System.Drawing.Point(545, 0);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(37, 39);
+            this.btnExit.TabIndex = 1;
+            this.tltMain.SetToolTip(this.btnExit, "Exit !");
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(102)))));
+            this.ClientSize = new System.Drawing.Size(582, 365);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblTodayResult);
+            this.Controls.Add(this.lblToday);
+            this.Controls.Add(this.dtpBirthday);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblBirthday);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.pnlTop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.pnlTop.ResumeLayout(false);
+            this.pnlTop.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlTop;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.ToolTip tltMain;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblBirthday;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.DateTimePicker dtpBirthday;
+        private System.Windows.Forms.Label lblToday;
+        private System.Windows.Forms.Label lblTodayResult;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int move, movx, movy;
+        public Form1()
+        {
+            InitializeComponent();
+            lblTodayResult.Text = DateTime.Now.ToString();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pnlTop_DoubleClick(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void pnlTop_MouseDown(object sender, MouseEventArgs e)
+        {
+            move = 1;
+            movx = e.X;
+            movy = e.Y;
+        }
+
+        private void dtpBirthday_ValueChanged(object sender, EventArgs e)
+        {
+            if (dtpBirthday.Value > DateTime.Now)
+            {
+                MessageBox.Show("Error !! Cant Select After Today");
+            }
+        }
+
+        private void pnlTop_MouseUp(object sender, MouseEventArgs e)
+        {
+            move = 0;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+           string rDays= dateTimeBirthday(dtpBirthday.Value).Item1;
+            string rMonths =   dateTimeBirthday(dtpBirthday.Value).Item2;
+            string rYears =  dateTimeBirthday(dtpBirthday.Value).Item3;
+
+            lblResult.Text = $"Hello {txtName.Text} \n  Age is \n {rYears} years\n {rMonths} months\n {rDays} days";
+
+
+        }
+
+        private void pnlTop_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (move == 1)
+            {
+                this.SetDesktopLocation(MousePosition.X-movx, MousePosition.Y-movy);
+            }
+        }
+        public (string , string ,string) dateTimeBirthday (DateTime birthday)
+        {
+            birthday= dtpBirthday.Value;
+            DateTime today = DateTime.Today;
+            int months = today.Month - birthday.Month;
+            int years = today.Year - birthday.Year;
+            if (today.Day < birthday.Day)
+            {
+                months--;
+
+            }
+            if (today.Month < birthday.Month)
+            {
+                years--;
+                months  += 12;
+            }
+            
+            int days = (today - birthday.AddMonths((years * 12 )+ months)).Days;
+            
+            return (days.ToString(),months.ToString(),years.ToString());
+        }
+    }
+}
